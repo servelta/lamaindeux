a12a8bf15455dbde44e71600d046388a93611ae2
servelta <servelta0@gmail.com>
2026-08-20 13:10:01 +0200

    Update calendar-manager.tsx

--- components/professional/calendar-manager.tsx
+++ components/professional/calendar-manager.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useActionState, useTransition } from "react";
+import { useFormState as useActionState } from "react-dom";
+import { useTransition } from "react";
 import { X } from "lucide-react";
 import {
   addAvailabilitySlotAction,
